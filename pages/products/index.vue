811824aac18a14f136bdffc1e1f1eacd062f2385
ThomasvHoutum <thomasvanhoutum@hotmail.com>
2024-04-27 18:49:58 +0200

    init

---
+++ pages/products/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="product in products" :key="product.id">
+        <ProductCard :product="product"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProductCard from "~/components/ProductCard.vue";
+
+definePageMeta({
+  layout: 'products'
+})
+
+const {data: products} = await useFetch('https://fakestoreapi.com/products')
+
+useHead({
+  title: 'Nuxt App | Products',
+  meta: [
+    {name: "description", content: "Nuxt 3 Products"}
+  ]
+})
+</script>
+
+<style scoped>
+
+</style>
